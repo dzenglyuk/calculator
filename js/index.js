33a8486f7f8a20550229bf009190e7418d2520c0
Dzenhliuk Oleh <dzenglyuk23@ukr.net>
2018-05-07 14:02:38 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,47 +1,34 @@
 (function() {
 
-    let basicButtons = document.getElementsByClassName('basic-buttons');
+    const basicButtons = document.querySelectorAll('.basic-buttons');
 
     for (let i = 0; i < basicButtons.length; i++) {
         basicButtons[i].addEventListener('click', function(e) {
-            e.preventDefault();
             const buttonValue = basicButtons[i].value;
             document.querySelector('.screen span').innerHTML += buttonValue;
         });
     }
-})();
 
-(function(){
-    
-    document.getElementsByClassName('clear-button')[0]
+    document.querySelector('.clear-button')
         .addEventListener('click', clearScreen);
         
     function clearScreen(e) {
-        e.preventDefault();
         document.querySelector('.screen span').innerHTML = "";
     }
-})(); 
-
-(function(){
-    
-    document.getElementsByClassName('correct-button')[0]
+ 
+    document.querySelector('.correct-button')
         .addEventListener('click', correctScreen);
         
     function correctScreen(e) {
-        e.preventDefault();
         let correction = document.querySelector('.screen span').innerHTML;
         correction = correction.slice(0, -1);
         document.querySelector('.screen span').innerHTML = correction;
     }
-})(); 
 
-(function(){
-    
-    document.getElementsByClassName('equal-button')[0]
+    document.querySelector('.equal-button')
         .addEventListener('click', getResult);
         
     function getResult(e) {
-        e.preventDefault();
         let result = eval(document.querySelector('.screen span').innerHTML);
         
         document.querySelector('.screen span').innerHTML = result;
